5952e94316089e343a425caabefa334519268b54
Damien PLENARD <damien@plenard.me>
2019-11-08 17:07:55 +0100

    Updates

---
+++ precache-manifest.d5335b8c71094849fe2ccb863f4a3f1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f0e7c67067920f222ba5c0694ecb711",
+    "url": "/index.html"
+  },
+  {
+    "revision": "875be3871b11124a3fc0",
+    "url": "/static/css/main.f00d3403.chunk.css"
+  },
+  {
+    "revision": "3684ecd2b4a211693127",
+    "url": "/static/js/2.9b68e09c.chunk.js"
+  },
+  {
+    "revision": "875be3871b11124a3fc0",
+    "url": "/static/js/main.85f14023.chunk.js"
+  },
+  {
+    "revision": "bc6d5072dd831050122f",
+    "url": "/static/js/runtime-main.90b3cf65.js"
+  }
+]);
