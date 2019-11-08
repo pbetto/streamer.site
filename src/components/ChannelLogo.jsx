f10b03b57d3332727818f7d6deb8147cbdbade60
Damien PLENARD <damien@plenard.me>
2019-11-08 18:17:17 +0100

    round corner of channel logo

--- src/components/ChannelLogo.jsx
+++ src/components/ChannelLogo.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 const Logo = styled.img`
   margin: 1em;
   width: calc(3vw + 150px);
+  border-radius: 12px;
 `;
 
 const ChannelLogo = props => <Logo src={props.user.profile_image_url} alt="" />;
